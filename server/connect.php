006e0adb50a0a4616b3e8573054ca1ef3c13eb69
Anh Duy <anhduyg07@gmail.com>
2021-11-04 16:46:47 +0700

    Cập nhật file connect.php

--- server/connect.php
+++ server/connect.php
@@ -1,8 +1,8 @@
 <?php
-    $servername = "localhost";
+    $nameserver = "localhost";
     $username = "root";
     $password = "";
     $database = "trasua";
-    $conn = mysqli_connect($servername,$username,$password,$database);
+    $conn = mysqli_connect($nameserver,$username,$password,$database);
     mysqli_query($conn,"SET NAMES 'utf8'");
 ?>
